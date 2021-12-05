fc01e503ddda9d1840b6ad2f51847ff378ab8754
paponExabyting <papon@exabyting>
2021-12-05 14:13:53 +0600

    make it better

---
+++ src/main/java/HerokuMain1.java
@@ -0,0 +1,9 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuMain1 {
+    public static void main(String[] args){
+        SpringApplication.run(HerokuMain1.class,args);
+    }
+}
